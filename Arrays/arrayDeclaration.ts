cfca611bf6015a0201d5702e0f2ed3f5b33f48a3
Simriti <simritib1@gmail.com>
2023-12-22 17:05:00 +0400

    Create arrayDeclaration.ts

---
+++ Arrays/arrayDeclaration.ts
@@ -0,0 +1,9 @@
+let names: string[] = ['Danny', 'Samantha'];
+let names: Array<string> = ['Danny', 'Samantha'];
+
+
+// Arrays:
+let bestNumbers: number[] = [7,77,4];
+let bestLunches: string[] = ['chicken soup', 'non-chicken soup'];
+let bestBreakfasts: string[] = ['scrambled eggs', 'oatmeal', 'tamago kake gohan', 'any kind of soup'];
+let bestBooleans: boolean[] = [true, false];
