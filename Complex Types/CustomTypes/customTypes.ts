b3e34252491b0c460163db64d2c7697bd3d3d05d
Simriti <simritib1@gmail.com>
2023-12-22 17:42:56 +0400

    Create customTypes.ts

---
+++ Complex Types/CustomTypes/customTypes.ts
@@ -0,0 +1,9 @@
+// Introduction
+let myVar: compType; 
+function testFn(param: compType): returnedCompType {
+  /*Function body*/
+}
+let inferredTypeVariable = testFn(myVar);
+// The variable inferredTypeVariable will have the type returnedCompType. 
+
+// ENUMS
