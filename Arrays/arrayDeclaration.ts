bb5947f84a2dcba98e86a1162722778ec4449dc3
Simriti <simritib1@gmail.com>
2023-12-22 17:07:22 +0400

    Delete Arrays directory

--- Arrays/arrayDeclaration.ts
+++
@@ -1,9 +0,0 @@
-let names: string[] = ['Danny', 'Samantha'];
-let names: Array<string> = ['Danny', 'Samantha'];
-
-
-// Arrays:
-let bestNumbers: number[] = [7,77,4];
-let bestLunches: string[] = ['chicken soup', 'non-chicken soup'];
-let bestBreakfasts: string[] = ['scrambled eggs', 'oatmeal', 'tamago kake gohan', 'any kind of soup'];
-let bestBooleans: boolean[] = [true, false];
