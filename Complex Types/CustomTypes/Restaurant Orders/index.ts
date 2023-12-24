00de76321900d42441f7a75e795e8776c8347a7f
Simriti <simritib1@gmail.com>
2023-12-24 08:52:59 +0400

    Create index.ts

---
+++ Complex Types/CustomTypes/Restaurant Orders/index.ts
@@ -0,0 +1,40 @@
+import { restaurants, Restaurant } from "./restaurants";
+import { orders, Order, PriceBracket } from "./orders";
+
+/// Add your getMaxPrice() function below:
+function getMaxPrice(maxPrice: PriceBracket): number {
+  switch (maxPrice){
+    case PriceBracket.Low:
+      return 10.0;
+    case PriceBracket.Medium:
+      return 20.0;
+    case PriceBracket.High:
+      return 30.0;
+  }
+}
+
+/// Add your getOrders() function below:
+function getOrders(price: PriceBracket, orders: Order[][]) {
+  let filteredOrders: Order[][] = [];
+  orders.forEach(restaurant => {
+    const restaurantOrder = restaurant.filter(order => order.price <= getMaxPrice(price));
+    filteredOrders.push(restaurantOrder);
+  });
+  return filteredOrders;  
+}
+/// Add your printOrders() function below:
+function printOrders(restaurants: Restaurant[], filteredOrders: Order[][]) {
+filteredOrders.forEach((order, index) => {
+    if (order.length > 0) {
+      console.log(`${restaurants[index].name}`);
+      order.forEach(item => {
+        console.log(`- ${item.name}: $${item.price}`);
+      });
+    }
+  });
+}
+
+
+/// Main
+const elligibleOrders = getOrders(PriceBracket.Low, orders);
+printOrders(restaurants, elligibleOrders);
